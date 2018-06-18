6dbd36f6f8bed510dae6885160e1f06afc6d8940
Alex Basson <alexbasson@gmail.com>
2018-06-18 17:11:56 -0400

    Add Oven to SmartHome

---
+++ Oven/src/main/java/com/example/oven/Oven.java
@@ -0,0 +1,6 @@
+package com.example.oven;
+
+public class Oven {
+    public int in(int location) { return 0; }
+    public void out(int location, boolean on) {}
+}
---
+++ src/main/java/com/example/smarthome/SmartKitchen.java
@@ -0,0 +1,26 @@
+package com.example.smarthome;
+
+import com.example.oven.Oven;
+
+public class SmartKitchen {
+    void regulateTemp() {
+        int goalT, t, s;
+
+        while (true) {
+            Oven o = new Oven();
+
+            goalT = o.in(0x02);
+            t = o.in(0x01);
+            s = o.in(0x03);
+
+            if (s == 1){
+                if (t < goalT) {
+                    o.out(0x04, true);
+                } else {
+                    o.out(0x04, false);
+                }
+            }
+        }
+    }
+
+}
